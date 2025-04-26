567d76a2cf4cca4358d78f8a97eb11d09b015c38
Anna Zhu <annazhu@usc.edu>
2025-04-26 00:23:19 +0000

    fix

--- wordle.cpp
+++ wordle.cpp
@@ -13,69 +13,106 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-map<char, int> countChars(const string& s){
-  map<char, int> counts;
-  for (char c : s){
-    counts[c]++;
-  }
-  return counts;
-}
-
 void wordleHelper(const std::string& in,
-    const std::map<char, int>& floatCounts,
+    //const std::set<char> floating,
+    string& current,
+    int index,
+    int floatCounts[26],
+    int floatLeft,
     const std::set<std::string>& dict,
-    string current,
-    size_t ind,
-    set<string>& results,
-    const set<string>& dictPrefixes)
+    set<string>& results)
 {
-    int remainingPositions = in.size() - ind;
-    int requiredFloats = 0;
-
-    for (const auto& pair : floatCounts){
-      requiredFloats += pair.second;
+  /*
+    int remainingPos = in.size() - ind;
+    if (floating.size() > remainingPos) {
+        return;
     }
-    if (requiredFloats > remainingPositions){
-      return;
-    }
-
-    if (ind == in.size()){
-      if (requiredFloats == 0 && dict.find(current) != dict.end()){
-        results.insert(current);
-      }
-      return;
-    }
-
-    if (in[ind] != '-'){
-      wordleHelper(in, floatCounts, dict, current + in[ind], ind + 1, results, dictPrefixes);
-      return;
-    }
-    for (const auto& pair : floatCounts){
-      if (pair.second > 0){
-        char c = pair.first;
-        map<char, int> newFloatCounts = floatCounts;
-        newFloatCounts[c]--;
-        if (newFloatCounts[c] == 0){
-          newFloatCounts.erase(c);
+  */
+    if (index == in.size()){
+      if (floatLeft == 0 && dict.find(current) != dict.end()) {
+            results.insert(current);
         }
-        string newPrefix = current + c;
-        if (dictPrefixes.find(newPrefix) != dictPrefixes.end()){
-          wordleHelper(in, newFloatCounts, dict, newPrefix, ind + 1, results, dictPrefixes);
+        return;
+    }
+
+    /*
+    size_t remaining = in.size() - ind;
+    if (floating.size() > remaining){
+      return;
+    }
+    */
+
+    if (in[index] != '-'){
+      current[index] = in[index];
+      //current.push_back(in[ind]);
+      wordleHelper(in, current, index + 1, floatCounts, floatLeft, dict, results);
+      //current.pop_back();
+      return;
+    }
+
+    for (int i = 0; i < 26; ++i){
+      if (floatCounts[i] > 0){
+        current[index] = 'a' + i;
+        floatCounts[i]--;
+        wordleHelper(in, current, index + 1, floatCounts, floatLeft - 1, dict, results);
+        floatCounts[i]++;
+      }
+    }
+    int remainingSlots = in.size() - index - 1;
+    if (remainingSlots >= floatLeft){
+      for (int i = 0; i < 26; ++i){
+        if (floatCounts[i] == 0){
+          current[index] = 'a' + i;
+          wordleHelper(in, current, index + 1, floatCounts, floatLeft, dict, results);
         }
       }
     }
-    
-    if (remainingPositions > requiredFloats){
+    /*
+    std::set<char> tried; 
+    for (auto it = floating.begin(); it != floating.end(); ++it) {
+        if (tried.count(*it)) continue;
+        tried.insert(*it);
+
+        char c = *it;
+        current.push_back(c);
+
+        std::multiset<char> newFloating = floating;
+        newFloating.erase(newFloating.find(c));         
+        wordleHelper(in, newFloating, dict, current, ind + 1, results);
+
+        current.pop_back();
+    }
+    if (floating.size() < remaining) {
+        for (char c = 'a'; c <= 'z'; ++c) {
+            if (floating.count(c)) continue; // already tried above
+
+            current.push_back(c);
+            wordleHelper(in, floating, dict, current, ind + 1, results);
+            current.pop_back();
+        }
+    }
+
+    //bool used[26] = {false};
+    for (size_t i = 0; i < floating.size(); ++i) {
+        char c = floating[i];
+        string newFloating = floating.substr(0, i) + floating.substr(i + 1);
+        //current.push_back(c);
+        wordleHelper(in, newFloating, dict, current + c, ind + 1, results);
+        //current.pop_back();
+    }
+  
+    //int remainingPos = in.size() - ind - 1;
+    if (in.size() - ind > floating.size()){
       for (char c = 'a'; c <= 'z'; ++c){
-        if (floatCounts.find(c) != floatCounts.end()){
+        if (floating.find(c) != string::npos) {
           continue;
         }
-        string newPrefix = current + c;
-        if (dictPrefixes.find(newPrefix) != dictPrefixes.end()){
-          wordleHelper(in, floatCounts, dict, newPrefix, ind + 1, results, dictPrefixes);
-        }
+        //current.push_back(c);
+        wordleHelper(in, floating, dict, current + c, ind + 1, results);
+        //current.pop_back();
       }
     }
+    */
 }
 
 // Definition of primary wordle function
@@ -86,14 +123,14 @@ std::set<std::string> wordle(
 {
     // Add your code here
     set<string> results;
-    map<char, int> floatCounts = countChars(floating);
-    set<string> dictPrefixes;
-    for (const string& word :dict){
-      for (size_t len = 1; len <= word.size(); ++len){
-        dictPrefixes.insert(word.substr(0, len));
-      }
+    string current = in;
+    int floatCounts[26] = {0};
+    for (char c : floating){
+      floatCounts[c - 'a']++;
     }
-    wordleHelper(in, floatCounts, dict, "", 0, results, dictPrefixes);
+    //multiset<char> inFloat(floating.begin(), floating.end());
+    //current.reserve(in.size());
+    wordleHelper(in, current, 0, floatCounts, floating.size(), dict, results);
     return results;
 }
 
